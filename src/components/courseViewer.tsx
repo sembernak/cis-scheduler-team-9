5c82c89410697095f0bdc929ef7e46f041f6bdac
aplatchek <platchek@udel.edu>
2022-04-07 15:07:09 -0400

    made courseViewer

--- src/components/courseViewer.tsx
+++ src/components/courseViewer.tsx
@@ -1,2 +1,28 @@
-export {};
-//CourseViewer
+import React from "react";
+import { Col } from "react-bootstrap";
+import { Course } from "../interfaces/course";
+
+export function courseViewer({
+    course
+}: {
+    changeView: () => void;
+    course: Course;
+}): JSX.Element {
+    const preRecs = course.prereq.join(", ");
+    return (
+        <>
+            <Col>
+                <h3>
+                    {course.code} {" - "} {course.title}
+                    <br></br>
+                    {course.credits} {" credits"}
+                    <br></br>
+                    {course.description}
+                    <br></br>
+                    {preRecs.length === 0 ? "" : "Prerequisites: " + preRecs}
+                </h3>
+            </Col>
+            <div></div>
+        </>
+    );
+}
