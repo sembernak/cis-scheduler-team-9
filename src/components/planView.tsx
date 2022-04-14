4b3975cd589692e48fd4dee815402f01e9c632a4
sammyb102 <samcori@udel.edu>
2022-04-14 13:37:07 -0400

    revised layout

--- src/components/planView.tsx
+++ src/components/planView.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Semester } from "../interfaces/semester";
-import { Button, Stack } from "react-bootstrap";
+import { Button, Container, Col, Row } from "react-bootstrap";
 import { SemesterView } from "./semesterView";
 import { Plan } from "../interfaces/plan";
 import { Course } from "../interfaces/course";
@@ -32,22 +32,26 @@ export function PlanView({
     return (
         <div>
             <h3>{plan.title}</h3>
-            <Stack gap={3}>
-                {plan.semesters.map((Semester: Semester) => (
-                    <div key={Semester.id} className="bg-light border m-2 p-2">
-                        <SemesterView
-                            semester={Semester}
-                            deleteSemester={deleteSemester}
-                            deleteCourse={deleteCourse}
-                            //viewSemester={viewSemester}
-                            editCourse={editCourse}
-                            editSemester={editSemester}
-                            //viewing={viewing}
-                            //setViewing={setViewing}
-                        ></SemesterView>
-                    </div>
-                ))}
-            </Stack>
+            <Container>
+                <Row md={2} sm={1}>
+                    {plan.semesters.map((Semester: Semester) => (
+                        <Col className={"row-equal"} key={Semester.id}>
+                            <div className="bg-light border m-2 p-2 contains-semester">
+                                <SemesterView
+                                    semester={Semester}
+                                    deleteSemester={deleteSemester}
+                                    deleteCourse={deleteCourse}
+                                    //viewSemester={viewSemester}
+                                    editCourse={editCourse}
+                                    editSemester={editSemester}
+                                    //viewing={viewing}
+                                    //setViewing={setViewing}
+                                ></SemesterView>
+                            </div>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
             <Button
                 onClick={() => deletePlan(plan.id)}
                 variant="danger"
