983c2cda239598312f3ce1a5769f9aca9d53c006
anissaspano <90479879+anissaspano@users.noreply.github.com>
2022-05-16 12:06:34 -0400

    retry

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -891,7 +891,7 @@ describe("Make a new plan", () => {
             screen.getByLabelText("Season:"),
             // Find and select the Ireland option, like a real user would.
             screen.getByRole("option", { name: "Summer" })
-    );
+        );
         userEvent.type(setYear, "2010");
         screen.getByRole("button", { name: "Save", hidden: false }).click();
         expect(screen.getAllByText(/Summer - 2010/i)).toBeInTheDocument; //initially semester is in the document
@@ -916,7 +916,7 @@ describe("Make a new plan", () => {
             .click(); //saving semester
         window.location.reload(); //refresh page
         expect(editedSem[0]).toBeInTheDocument; //semester still visible
-      });
+    });
     test("Welcome message is pulled up when help button is pressed", () => {
         const closeButton = screen.getByRole("button", { name: /close/i });
         closeButton.click(); //close the welcome message
